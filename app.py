96f3cf3450f60c9b044279b9a6f244c19ccb5fd8
Henning Jacobs <henning@zalando.de>
2018-07-01 00:03:00 +0200

    type hints

--- app.py
+++ app.py
@@ -18,30 +18,31 @@ REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, socket_timeout=5)
 
 
-def get_redis_key(pet_id):
+def get_redis_key(pet_id: str) -> str:
     return "pets:{}".format(pet_id)
 
 
-def get_pets(limit, animal_type=None):
+def get_pets(limit: int, animal_type=None):
+    # TODO: filter by animal_type
     keys = r.keys(get_redis_key("*"))
     return {
         "pets": [_get_pet(key.decode("utf-8").split(":")[1]) for key in keys][:limit]
     }
 
 
-def _get_pet(pet_id):
+def _get_pet(pet_id: str) -> dict:
     pet = r.get(get_redis_key(pet_id))
     if pet:
         pet = json.loads(pet)
     return pet
 
 
-def get_pet(pet_id):
+def get_pet(pet_id: str):
     pet = _get_pet(pet_id)
     return pet or connexion.problem(404, "Not found", "Pet not found")
 
 
-def put_pet(pet_id, pet):
+def put_pet(pet_id: str, pet):
     exists = _get_pet(pet_id)
     pet["id"] = pet_id
     if exists:
@@ -55,7 +56,7 @@ def put_pet(pet_id, pet):
     return NoContent, (200 if exists else 201)
 
 
-def delete_pet(pet_id):
+def delete_pet(pet_id: str):
     exists = _get_pet(pet_id)
     if exists:
         logging.info("Deleting pet %s..", pet_id)
