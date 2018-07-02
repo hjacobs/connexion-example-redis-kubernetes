087ce3949eb5fd5350e5d4fda42e6200ddfbe4ed
Henning Jacobs <henning@jacobs1.de>
2018-07-02 22:33:44 +0200

    more generic use

--- vegeta-plot.py
+++ vegeta-plot.py
@@ -9,6 +9,44 @@ CHARS = ' ▁▂▃▄▅▆▇█'
 UNKNOWN = '▒'
 
 
+def refresh(data, height):
+    ymin = float('+inf')
+    ymax = float('-inf')
+    for record in data:
+        yvalue = record[min(2, len(record)-1)]
+        ymin = min(ymin, yvalue)
+        ymax = max(ymax, yvalue)
+    if ymin == ymax:
+        ymax = ymin + 0.1
+    click.clear()
+    for y in range(height-1, -1, -1):
+        label = ymin + (ymax - ymin) * y
+        print('{:.2f}ms ┤'.format(label/(1000**2)).rjust(16), end='')
+        for record in data:
+            yvalue = record[min(2, len(record)-1)]
+            if len(record) > 2:
+                status = record[1]
+            else:
+                status = 200
+            if status >= 200 and status < 400:
+                color = 'green'
+            elif status >= 400 and status < 500:
+                color = 'yellow'
+            else:
+                color = 'red'
+            val = 1. * (yvalue - ymin)/(ymax - ymin) * height
+            idx = min(max(round(len(CHARS) * (val - y)), 0), len(CHARS)-1)
+            char = CHARS[idx]
+            if yvalue == 0:
+                char = UNKNOWN
+            click.secho(char, nl=False, fg=color)
+        print()
+    print(' ' * 16, end='')
+    for i in range(len(data)):
+        print('┬', end='')
+    print()
+
+
 @click.command()
 @click.option('-d', '--delimiter', default=',')
 @click.option('-h', '--height', default=40, type=int)
@@ -21,39 +59,9 @@ def main(delimiter, height):
         data.append(record)
         if time.time() < last_refresh + 0.2:
             continue
-        ymin = float('+inf')
-        ymax = float('-inf')
-        for record in data:
-            yvalue = record[2]
-            ymin = min(ymin, yvalue)
-            ymax = max(ymax, yvalue)
-        if ymin == ymax:
-            ymax = ymin + 0.1
-        click.clear()
-        for y in range(height-1, -1, -1):
-            label = ymin + (ymax - ymin) * y
-            print('{:.2f}ms ┤'.format(label/(1000**2)).rjust(16), end='')
-            for record in data:
-                yvalue = record[2]
-                status = record[1]
-                if status >= 200 and status < 400:
-                    color = 'green'
-                elif status >= 400 and status < 500:
-                    color = 'yellow'
-                else:
-                    color = 'red'
-                val = 1. * (yvalue - ymin)/(ymax - ymin) * height
-                idx = min(max(round(len(CHARS) * (val - y)), 0), len(CHARS)-1)
-                char = CHARS[idx]
-                if yvalue == 0:
-                    char = UNKNOWN
-                click.secho(char, nl=False, fg=color)
-            print()
-        print(' ' * 16, end='')
-        for i in range(100):
-            print('┬', end='')
-        print()
+        refresh(data, height)
         last_refresh = time.time()
+    refresh(data, height)
 
 
 if __name__ == '__main__':
